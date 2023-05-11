ff4f7c107b25a0ff914e2ce9dab9f5e5af3b2146
Surender Singh. Bhandari <surender.bhandari@ummeedhfc.com>
2023-05-11 17:59:14 +0530

    update yaml files

---
+++ deploy_war.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+echo "Deploying WAR file to Tomcat server..."
+
+# Stop Tomcat server
+sudo systemctl stop tomcat
+
+# Remove old deployment artifacts
+sudo rm -rf /opt/tomcat/webapps/gs-*
+
+# Copy new WAR file to Tomcat webapps directory
+sudo cp /opt/myapp/artifacts/gs-*.jar /opt/tomcat/webapps/
+
+# Start Tomcat server
+sudo systemctl start tomcat
+
+echo "Deployment complete."
