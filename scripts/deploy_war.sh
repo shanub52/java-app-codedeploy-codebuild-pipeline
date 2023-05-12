d85d413f0353c8f82799f7c657c9bd13ec9628b8
Surender Singh. Bhandari <surender.bhandari@ummeedhfc.com>
2023-05-12 14:16:02 +0530

    udpate testing

--- scripts/deploy_war.sh
+++ scripts/deploy_war.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Define the path to the WAR file
-WAR_FILE=/opt/myapp/gs-spring-boot-0.1.0.jar
+WAR_FILE=/opt/myapp/gs-spring-boot-0.1.0.war
 
 # Define the path to the Tomcat installation directory
 TOMCAT_DIR=/opt/tomcat
@@ -11,7 +11,7 @@ TOMCAT_DIR=/opt/tomcat
 sudo systemctl stop tomcat
 
 # Remove the old application files
-sudo rm -rf $TOMCAT_DIR/webapps/gs-spring-boot-0.1.0.jar
+sudo rm -rf $TOMCAT_DIR/webapps/gs-spring-boot-0.1.0.war
 
 # Copy the new WAR file to the Tomcat webapps directory
 sudo cp $WAR_FILE $TOMCAT_DIR/webapps/
