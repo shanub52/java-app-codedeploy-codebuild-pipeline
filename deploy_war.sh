fc5b94b3ef751f3fc69cecc5b3a618f45e817041
Surender Singh. Bhandari <surender.bhandari@ummeedhfc.com>
2023-05-12 13:20:58 +0530

    udpate testing

--- deploy_war.sh
+++ deploy_war.sh
@@ -7,10 +7,10 @@ echo "Deploying WAR file to Tomcat server..."
 sudo systemctl stop tomcat
 
 # Remove old deployment artifacts
-sudo rm -rf /opt/tomcat/webapps/gs-spring-boot-0.1.0.jar
+sudo rm -rf /opt/tomcat/webapps/gs-spring-boot-0.1.0.war
 
 # Copy new WAR file to Tomcat webapps directory
-sudo cp /opt/myapp//gs-spring-boot-0.1.0.jar /opt/tomcat/webapps/
+sudo cp /opt/myapp//gs-spring-boot-0.1.0.war /opt/tomcat/webapps/
 
 # Start Tomcat server
 sudo systemctl start tomcat
