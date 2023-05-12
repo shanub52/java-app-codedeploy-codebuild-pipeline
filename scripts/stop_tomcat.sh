5567b6ebbb7b8504c025049300a9a1320a866bfc
Surender Singh. Bhandari <surender.bhandari@ummeedhfc.com>
2023-05-12 17:29:11 +0530

    update

--- scripts/stop_tomcat.sh
+++ scripts/stop_tomcat.sh
@@ -1,9 +1,2 @@
 #!/usr/bin/env bash
-set -x
-APP_HOME="/opt/tomcat"
-ifAppRunning=`pgrep java`
-if [[ -n  $ifAppRunning ]]; then
-   sudo killall java 
-fi
-echo "listing files..."
-ls -l $APP_HOME/
+sudo systemctl stop tomcat
